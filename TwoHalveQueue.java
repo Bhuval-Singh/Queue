e1d482c7036d0dc5adbb05cb9d26e9dd4155d625
Bhuval-Singh <ayushsinghbca2022@gmail.com>
2024-09-24 19:22:23 +0530

    24 sept

---
+++ TwoHalveQueue.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class TwoHalveQueue {
+    
+    public static void main(String[] args) {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        q.add(4);
+        q.add(5);
+        q.add(6);
+        q.add(7);
+        q.add(8);
+        q.add(9);
+        q.add(10);
+    }
+}
