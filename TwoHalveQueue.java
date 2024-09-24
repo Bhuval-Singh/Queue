3cf0579a3663e0f1001da17f3045e19950b5ef06
Bhuval-Singh <ayushsinghbca2022@gmail.com>
2024-09-24 19:33:21 +0530

    24 Sept

--- TwoHalveQueue.java
+++ TwoHalveQueue.java
@@ -1,6 +1,16 @@
 import java.util.*;
 public class TwoHalveQueue {
-    
+    public static void interLeave(Queue<Integer> q){
+        Queue<Integer> firstHalf = new LinkedList<>();
+        int size = q.size();
+        for(int i=0;i<size/2;i++){
+            firstHalf.add(q.remove());
+        }
+        while (!firstHalf.isEmpty()) {
+            q.add(firstHalf.remove());
+            q.add(q.remove());
+        }
+    }
     public static void main(String[] args) {
         Queue<Integer> q = new LinkedList<>();
         q.add(1);
@@ -13,5 +23,12 @@ public class TwoHalveQueue {
         q.add(8);
         q.add(9);
         q.add(10);
+        q.add(11);
+        q.add(12);
+        interLeave(q);
+        while (!q.isEmpty()) {
+            System.out.print(q.remove()+" ");
+        }
+        System.out.println();
     }
 }
